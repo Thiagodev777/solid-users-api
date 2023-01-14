e2c2390e99f3c9f3d418cdc1d57c403f5667593e
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-14 18:11:24 -0300

    add middleware error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,26 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: [".eslintrc.cjs"],
+  ignorePatterns: ['.eslintrc.cjs'],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-types": "off",
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-types': 'off',
   },
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes/routes'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -10,6 +11,20 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  console.log(error)
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}!`),
