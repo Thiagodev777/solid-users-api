08d27eb3845a9fc916a72fc999941f8eba3193aa
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-14 19:48:32 -0300

    creation of the findByName method in the Roles repository

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,9 +1,9 @@
 import { v4 as uuidv4 } from 'uuid'
 
 export class Role {
-  private id?: string
-  private name: string
-  private created_at: Date
+  public id?: string
+  public name: string
+  public created_at: Date
 
   constructor() {
     if (!this.id) {
--- src/roles/routes/roles.routes.ts
+++ src/roles/routes/roles.routes.ts
@@ -4,12 +4,20 @@ import { Router, Request, Response } from 'express'
 const rolesRouter = Router()
 const rolesRepository = new RolesRepository()
 
+rolesRouter.get('/', (req: Request, res: Response) => {
+  const roles = rolesRepository.findAll()
+  return res.status(200).json(roles)
+})
+
 rolesRouter.post('/', (req: Request, res: Response) => {
   const { name } = req.body
 
-  const role = rolesRepository.create({ name })
+  const roleAlreadyExists = rolesRepository.findByName(name)
+  if (roleAlreadyExists) {
+    return res.status(400).json({ error: 'Role already exists' })
+  }
 
+  const role = rolesRepository.create({ name })
   return res.status(201).json({ role })
 })
-
 export { rolesRouter }
