e105a4328dc517f1138f9171f616477b99519a97
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-17 19:52:25 -0300

    config database connection

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,15 @@
 import 'dotenv/config'
-import express, { Request, Response, NextFunction } from 'express'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-import cors from 'cors'
-import { routes } from './routes/routes'
-import { AppError } from './errors/AppError'
-import swaggerFile from './swagger.json'
+import 'reflect-metadata'
+import { app } from './app'
+import { dataSource } from './typeorm/'
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    })
-  }
-  console.log(error)
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server started on port ${process.env.PORT}!`),
+    )
   })
-})
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT}!`),
-)
+  .catch(err => {
+    console.log(err)
+  })
